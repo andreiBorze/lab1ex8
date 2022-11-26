22c1f5a65ce4e60387750d5cbde7d47328609500
Borze Andrei <borzeandrew@gmail.com>
2022-11-26 21:58:32 +0200

    Add project files.

---
+++ lab1ex8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab1ex8
+{
+    /*Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica
+    daca x este divizibil cu y.
+     In cazul in care x este divizibil cu y, programul va afisa “divizibil” iar in caz contrar,
+    “indivizibil”.*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acest program va verifica daca x este divizibil cu y. \n");
+
+            Console.WriteLine("Introduceti valoarea x:");
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea y:");
+            var y = int.Parse(Console.ReadLine());
+
+            if (x % y == 0)
+                Console.WriteLine("x divizibil cu y ");
+            else
+                Console.WriteLine("x indivizibil cu y");
+        }
+    }
+}
